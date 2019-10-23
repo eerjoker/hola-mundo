f06e9e66ac73130637ba65112182b176250a90ea
eerjoker <rivera.esteban1994@gmail.com>
2019-10-23 17:52:40 -0300

    Angular: ngClass, ngTemplate y Routing

--- aprendiendoAngular/src/app/app.module.ts
+++ aprendiendoAngular/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
     ZapatillasComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
   ], // carga componentes, pipes, directivas
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ], // carga los modulos
-  providers: [], //carga servicios
+  providers: [
+    appRoutingProviders
+  ], //carga servicios
   bootstrap: [AppComponent] // carga el componente principal
 })
 export class AppModule { }
--- aprendiendoAngular/src/app/cursos/cursos.component.ts
+++ aprendiendoAngular/src/app/cursos/cursos.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'cursos',
-  templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+	selector: 'cursos',
+	templateUrl: './cursos.component.html',
+	styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+	public nombre: string;
+	public followers: number;
 
-  constructor() { }
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this._route.params.subscribe((params: Params) => {
+			this.nombre = params.nombre;
+			//this.nombre = params['nombre'];
+			this.followers = +params.followers; // el + lo convierte en tipo number
+			
+			if(this.nombre == 'ninguno'){
+				this._router.navigate(['/home']);
+			}
+		})
+	}
+
+	redirigir(){
+		this._router.navigate(['/zapatillas']);
+	}
 
 }
